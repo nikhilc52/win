674f940ac7f5348622453f42df41dd20944776f8
nikhilc52 <nsc49@cornell.edu>
2025-11-05 18:25:14 -0500

    Added tooltip for turning information

---
+++ tooltip.js
@@ -0,0 +1,26 @@
+function fitTooltip() {
+    const rect = document.getElementById('page-1').getBoundingClientRect()
+
+    const tooltip = document.getElementById('turn-tooltip')
+    tooltip.style.top = rect.top + 15 + "px"
+    tooltip.style.left = rect.right + 55 + "px"
+
+    const tooltip_line = document.getElementById('turn-tooltip-line')
+    tooltip_line.style.top = rect.top + 32 + "px"
+    tooltip_line.style.left = rect.right - 25 + "px"
+    const tooltip_left = tooltip.style.left.substring(0, tooltip.style.left.length - 2)
+    tooltip_line.style.width = tooltip_left - rect.right + 25 + "px"
+}
+
+
+window.addEventListener('resize', fitTooltip) //don't think this is needed with hiding on click, but doesn't hurt
+fitTooltip()
+
+function hideTooltips() {
+    const tooltip_class = Array.from(this.document.getElementsByClassName('tooltip'))
+    tooltip_class.forEach((e) => e.style.visibility = 'hidden')
+}
+
+addEventListener('click', hideTooltips)
+addEventListener('keydown', hideTooltips)
+
--- utility.js
+++ utility.js
@@ -1,10 +1,10 @@
 function updateBarFromBar() {
-    var bar = document.getElementById('progress_bar')
+    const bar = document.getElementById('progress_bar')
     bar.style.background = `linear-gradient(to right, #ffdf0169 ${100 * bar.value / bar.max}%, #d3d3d3 ${100 * bar.value / bar.max}%)`
 }
 
 function updateBarFromPage() {
-    var bar = document.getElementById('progress_bar')
+    const bar = document.getElementById('progress_bar')
     bar.value = $('#flipbook').turn('view')[0]
     bar.style.background = `linear-gradient(to right, #ffdf0169 ${100 * bar.value / bar.max}%, #d3d3d3 ${100 * bar.value / bar.max}%)`
 }
@@ -13,22 +13,25 @@ $('#flipbook').bind('turned', updateBarFromPage);
 
 document.getElementById('l-arrow').addEventListener('click', function () {
     $('#flipbook').turn('previous');
-}
-)
+})
 
 document.getElementById('l-arrow-end').addEventListener('click', function () {
     $('#flipbook').turn('page', 1);
-}
-)
+})
 
 document.getElementById('r-arrow').addEventListener('click', function () {
     $('#flipbook').turn('next');
-}
-)
+})
 
 document.getElementById('r-arrow-end').addEventListener('click', function () {
     $('#flipbook').turn('page', 30);
-}
-)
+})
 
-
+addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowRight'){
+        $('#flipbook').turn('next');
+    }
+    if (event.key === 'ArrowLeft'){
+        $('#flipbook').turn('previous');
+    }
+})
