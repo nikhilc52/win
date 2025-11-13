242a735838e4f402d9e4e83e7ffae0ea58fb15bb
nikhilc52 <nsc49@cornell.edu>
2025-11-12 22:29:06 -0500

    Updated book with changes to page format on mobile

--- website/sizing.js
+++ website/sizing.js
@@ -2,11 +2,11 @@ function resizeBook() {
     const h = window.innerHeight
     const w = window.innerWidth
     if (h > w) {
-        $("#flipbook").turn('display','single')
+        $("#flipbook").turn('display', 'single')
         $("#flipbook").turn('size', 0.65 * window.innerWidth, 2 * 0.65 * 0.65 * window.innerWidth)
     }
     else {
-        $("#flipbook").turn('display','double')
+        $("#flipbook").turn('display', 'double')
         $("#flipbook").turn('size', 0.65 * window.innerWidth, 0.65 * 0.65 * window.innerWidth)
     }
 }
@@ -15,6 +15,12 @@ function setBookSize() {
     const h = window.innerHeight
     const w = window.innerWidth
     if (h > w) {
+        //make all pages soft for mobile viewing
+        //not possible to do when resizing after loading, very small issue
+        $("#page-1").removeClass('hard')
+        $("#page-2").removeClass('hard')
+        $("#page-31").removeClass('hard')
+        $("#page-32").removeClass('hard')
         $("#flipbook").turn({
             width: 0.65 * window.innerWidth,
             height: 2 * 0.65 * 0.65 * window.innerWidth,
